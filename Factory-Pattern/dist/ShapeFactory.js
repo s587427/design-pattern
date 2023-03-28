e7757de3a506d895229a7ef4eecf5fbe1ed79510
s587427 <98218224+s587427@users.noreply.github.com>
2023-03-28 12:39:21 +0800

    對於 Node.js 中的 ES6 模塊，您需要在導入語句中包含完整的文件路徑，包括文件擴展名

--- Factory-Pattern/dist/ShapeFactory.js
+++ Factory-Pattern/dist/ShapeFactory.js
@@ -1,23 +1,21 @@
-import { Circle } from "./Circle";
-import { Rectangle } from "./Rectangle";
-import { Square } from "./Square";
+import { Circle } from "./Circle.js"
+import { Rectangle } from "./Rectangle.js"
+import { Square } from "./Square.js"
 export class ShapeFactory {
-    getShape(shapeType) {
-        if (shapeType == null) {
-            return null;
-        }
-        if (equalsIgnoreCase(shapeType, "CIRCLE")) {
-            return new Circle();
-        }
-        else if (equalsIgnoreCase(shapeType, "RECTANGLE")) {
-            return new Rectangle();
-        }
-        else if (equalsIgnoreCase(shapeType, "SQUARE")) {
-            return new Square();
-        }
-        return null;
+  getShape(shapeType) {
+    if (shapeType == null) {
+      return null
     }
+    if (equalsIgnoreCase(shapeType, "CIRCLE")) {
+      return new Circle()
+    } else if (equalsIgnoreCase(shapeType, "RECTANGLE")) {
+      return new Rectangle()
+    } else if (equalsIgnoreCase(shapeType, "SQUARE")) {
+      return new Square()
+    }
+    return null
+  }
 }
 function equalsIgnoreCase(str1, str2) {
-    return str1.toLowerCase() === str2.toLowerCase();
+  return str1.toLowerCase() === str2.toLowerCase()
 }
--- Factory-Pattern/dist/index.js
+++ Factory-Pattern/dist/index.js
@@ -1,14 +1,15 @@
-import { ShapeFactory } from "./ShapeFactory";
-const shapeFactory = new ShapeFactory();
+// 對於 Node.js 中的 ES6 模塊，您需要在導入語句中包含完整的文件路徑，包括文件擴展名
+import { ShapeFactory } from "./ShapeFactory.js"
+const shapeFactory = new ShapeFactory()
 //獲取 Circle 的對象，並調用它的 draw 方法
-const shape1 = shapeFactory.getShape("CIRCLE");
+const shape1 = shapeFactory.getShape("CIRCLE")
 //調用 Circle 的 draw 方法
-shape1.draw();
+shape1.draw()
 //獲取 Rectangle 的對象，並調用它的 draw 方法
-const shape2 = shapeFactory.getShape("RECTANGLE");
+const shape2 = shapeFactory.getShape("RECTANGLE")
 //調用 Rectangle 的 draw 方法
-shape2.draw();
+shape2.draw()
 //獲取 Square 的對象，並調用它的 draw 方法
-const shape3 = shapeFactory.getShape("SQUARE");
+const shape3 = shapeFactory.getShape("SQUARE")
 //調用 Square 的 draw 方法
-shape3.draw();
+shape3.draw()
