5d2f662489dd9726c559d62a31c63bb57ab87b4e
s587427 <98218224+s587427@users.noreply.github.com>
2023-03-28 12:02:28 +0800

    Update ShapeFactory.ts

--- Factory-Pattern/src/ShapeFactory.ts
+++ Factory-Pattern/src/ShapeFactory.ts
@@ -1,25 +1,24 @@
-import { Circle } from "./Circle";
-import { Rectangle } from "./Rectangle";
-import { Square } from "./Square";
+import { Circle } from "./Circle"
+import { IShape } from "./IShape"
+import { Rectangle } from "./Rectangle"
+import { Square } from "./Square"
 
-export class ShapeFactory{
-    public getShape(shapeType: string){
-        if(shapeType == null){
-            return null;
-        }
-        if(equalsIgnoreCase(shapeType, "CIRCLE")){
-            return new Circle();
-        }else if(equalsIgnoreCase(shapeType, "RECTANGLE")){
-            return new Rectangle();
-        }else if(equalsIgnoreCase(shapeType, "SQUARE")){
-            return new Square();
-        }
-        return null;
-
+export class ShapeFactory {
+  public getShape(shapeType: string): IShape | null {
+    if (shapeType == null) {
+      return null
     }
+    if (equalsIgnoreCase(shapeType, "CIRCLE")) {
+      return new Circle()
+    } else if (equalsIgnoreCase(shapeType, "RECTANGLE")) {
+      return new Rectangle()
+    } else if (equalsIgnoreCase(shapeType, "SQUARE")) {
+      return new Square()
+    }
+    return null
+  }
 }
 
-
 function equalsIgnoreCase(str1: string, str2: string) {
-    return str1.toLowerCase() === str2.toLowerCase();
+  return str1.toLowerCase() === str2.toLowerCase()
 }
